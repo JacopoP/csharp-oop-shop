ed9c0232bfc780b46193e1884d995978d0b7a616
Jac22 <jacopo.prudentino@gmail.com>
2023-04-07 16:40:58 +0200

    Creato un array di prodotti e stampato il contenuto

--- Program.cs
+++ Program.cs
@@ -1,15 +1,29 @@
 ﻿using csharp_oop_shop;
 
-var prodotto1 = new Prodotto("Acqua", "Disseta molto", 10.99, 10);
-var prodotto2 = new Prodotto("Aria", "Riempie i polmoni", 1.13, 22);
+Prodotto[] prodotti = new Prodotto[]
+{
+new Prodotto("Acqua", "Disseta molto", 10.99, 10),
+new Prodotto("Aria", "Riempie i polmoni", 1.13, 22),
+new Prodotto("Humus", "Concima", 5.65, 22),
+new Prodotto("Diamanti", "Svuotano i portafigli", 9999.99, 22),
+new Prodotto("Pane", "Appena sfornato", 0.99, 4),
+new Prodotto("Libro Bello", "Nutre la mente", 16.50, 22),
+new Prodotto("Computer", "Fa calcoli", 1000.00, 22),
+};
 
-prodotto1.stampa();
-prodotto2.stampa();
+foreach (var prodotto in prodotti)
+{
+    prodotto.stampa();
+    Console.Write("\n");
+}
 
-prodotto1.nome = "Acqua frizzante";
-prodotto1.descrizione = "Disseta di più!";
-prodotto1.prezzo = 15.99;
-prodotto1.iva = 22;
+Console.WriteLine("Fine elenco");
 
-prodotto1.stampa();
+prodotti[0].nome = "Acqua frizzante";
+prodotti[0].descrizione = "Disseta di più!";
+prodotti[0].prezzo = 15.99;
+prodotti[0].iva = 22;
+
+Console.WriteLine("\n Prodotto modificato:");
+prodotti[0].stampa();
 
