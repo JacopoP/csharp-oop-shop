32c9b60c6e542cc719a6cd67a998f9c5721b6aee
Jac22 <jacopo.prudentino@gmail.com>
2023-04-07 15:49:09 +0200

    Created class Prodotto

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        public int codice { get; init; }
+        public string nome { get; set; }
+        public string descrizione { get; set; }
+        public double prezzo { get; set; }
+        public int iva { get; set; }
+
+        public Prodotto(string nome, string descrizione, double prezzo, int iva)
+        {
+            var rand = new Random();
+
+            codice = rand.Next(1000, 9999);
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+    }
+}
